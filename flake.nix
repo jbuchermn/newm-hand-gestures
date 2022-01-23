efc7b4459bff0a981231b4f1e4613e24a75c3c62
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-23 15:24:50 +0100

    PoC

--- flake.nix
+++ flake.nix
@@ -17,9 +17,8 @@
           (self: super: rec {
             python3 = super.python3.override {
               packageOverrides = self1: super1: {
-                opencv4 = (opencv4.override { enableGtk3 = true; });
                 mediapipe =  super.callPackage ./mediapipe.nix {
-                  inherit (python3Packages) buildPythonPackage isPy38 isPy39 absl-py attrs matplotlib numpy six wheel fetchPypi opencv4;
+                  inherit (python3Packages) buildPythonPackage isPy38 isPy39 absl-py attrs matplotlib numpy six wheel fetchPypi;
                   python = python3;
                 };
               };
@@ -28,14 +27,31 @@
           })
         ];
       };
+
+      dasbuspkg = {
+        dasbus = pkgs.python3.pkgs.buildPythonPackage rec {
+          pname = "dasbus";
+          version = "1.6";
+
+          src = pkgs.python3.pkgs.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-FJrY/Iw9KYMhq1AVm1R6soNImaieR+IcbULyyS5W6U0=";
+          };
+
+          setuptoolsCheckPhase = "true";
+
+          propagatedBuildInputs = with pkgs.python3Packages; [ pygobject3 ];
+        };
+      };
     in
     {
       devShell = let
         my-python = pkgs.python3;
         python-with-my-packages = my-python.withPackages (ps: with ps; [
+          (opencv4.override { enableGtk3 = true; })
           tensorflow
           mediapipe
-          opencv4
+          dasbuspkg.dasbus
 
           python-lsp-server
           pylsp-mypy
--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import time
 import cv2
 import matplotlib.pyplot as plt
 import mediapipe as mp
+from dasbus.connection import SessionMessageBus
+
+
+bus = SessionMessageBus()
+proxy = bus.get_proxy("org.newm.Gestures", "/org/newm/Gestures")
+
+def new_gesture():
+    res = proxy.New("swipe-3")
+    if res != "":
+        return bus.get_proxy("org.newm.Gestures.Gesture", res)
+    else:
+        return None
 
 cap = cv2.VideoCapture(0)
 
@@ -9,6 +21,10 @@ OUTPUT = True
 
 try:
     with mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+
+        current = None
+        init_counter = 0
+        init = 0., 0.
         while cap.isOpened():
             ret, frame = cap.read()
 
@@ -20,10 +36,26 @@ try:
             results = holistic.process(frame_rgb)
             dt = time.time() - t
 
-            print("dt=%f" % dt)
+            # print("dt=%f" % dt)
             if results.right_hand_landmarks is not None:
-                l = results.right_hand_landmarks.landmark[8]
-                print("x=%f, y=%f" % (l.x, l.y))
+                l = results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_TIP]
+                # print("x=%f, y=%f" % (l.x, l.y))
+
+                init_counter += 1
+                if init_counter > 3:
+                    if current is None:
+                        current = new_gesture()
+                        print("New")
+                        init = l.x, l.y
+                    else:
+                        print("Update")
+                        current.Update(["delta_x", "delta_y"], [- (l.x - init[0]), l.y - init[1]])
+            else:
+                init_counter = 0
+                if current is not None:
+                    print("Terminate")
+                    current.Terminate()
+                    current = None
 
             if OUTPUT:
                 mp.solutions.drawing_utils.draw_landmarks(frame, results.face_landmarks, mp.solutions.holistic.FACEMESH_CONTOURS)

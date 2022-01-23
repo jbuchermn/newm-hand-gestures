82e5484da7475e49457a645365fef95b647e5953
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-23 19:07:49 +0100

    Minor

--- flake.nix
+++ flake.nix
@@ -17,8 +17,9 @@
           (self: super: rec {
             python3 = super.python3.override {
               packageOverrides = self1: super1: {
-                mediapipe =  super.callPackage ./mediapipe.nix {
-                  inherit (python3Packages) buildPythonPackage isPy38 isPy39 absl-py attrs matplotlib numpy six wheel fetchPypi;
+                opencv4 = (super1.opencv4.override { enableGtk3 = true; });
+                mediapipe = super.callPackage ./mediapipe.nix {
+                  inherit (super1) buildPythonPackage isPy38 isPy39 absl-py attrs matplotlib numpy six wheel fetchPypi;
                   python = python3;
                 };
               };
@@ -48,7 +49,7 @@
       devShell = let
         my-python = pkgs.python3;
         python-with-my-packages = my-python.withPackages (ps: with ps; [
-          (opencv4.override { enableGtk3 = true; })
+          opencv4
           tensorflow
           mediapipe
           dasbuspkg.dasbus
--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def new_gesture():
         return None
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 2)
 
 OUTPUT = True
 
@@ -25,8 +26,15 @@ try:
         current = None
         init_counter = 0
         init = 0., 0.
+
+        counter = 0
         while cap.isOpened():
             ret, frame = cap.read()
+
+            # Hacky: Drop some frames to keep latency down
+            counter += 1
+            if counter%2 == 0:
+                continue
 
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
@@ -38,18 +46,31 @@ try:
 
             # print("dt=%f" % dt)
             if results.right_hand_landmarks is not None:
-                l = results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_TIP]
-                # print("x=%f, y=%f" % (l.x, l.y))
+                index = [
+                    results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_MCP],
+                    results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_PIP],
+                    results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_TIP],
+                ]
+                index = [(l.x, l.y) for l in index]
+                scalar = (index[2][0] - index[1][0]) * (index[1][0] - index[0][0]) + (index[2][1] - index[1][1]) * (index[1][1] - index[0][1])
+                print(scalar)
 
-                init_counter += 1
-                if init_counter > 3:
-                    if current is None:
-                        current = new_gesture()
-                        print("New")
-                        init = l.x, l.y
-                    else:
-                        print("Update")
-                        current.Update(["delta_x", "delta_y"], [- (l.x - init[0]), l.y - init[1]])
+                if scalar > 0:
+                    init_counter += 1
+                    if init_counter > 3:
+                        if current is None:
+                            current = new_gesture()
+                            print("New")
+                            init = index[0]
+                        else:
+                            print("Update")
+                            current.Update(["delta_x", "delta_y"], [- (index[0][0] - init[0]), index[0][1] - init[1]])
+                else:
+                    init_counter = 0
+                    if current is not None:
+                        print("Terminate")
+                        current.Terminate()
+                        current = None
             else:
                 init_counter = 0
                 if current is not None:
